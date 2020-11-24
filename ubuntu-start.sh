6d3ac72c8023dcb5c3a1d18411aa593739670500
zaharchenko-test <zaharchenko.jeka.v@gmail.com>
2020-11-24 18:16:28 +0100

    commit

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -1,20 +1,25 @@
 apt-get update -y && apt-get install wget curl proot tar -y && wget https://raw.githubusercontent.com/tuanpham-dev/termux-ubuntu/master/ubuntu.sh && bash ubuntu.sh
 
+
 sudo usermod -a -G sudo jeka
 
 
-
 sudo apt-get install -y apt-utils
-
 sudo apt-get install -y software-properties-common build-essential curl nano
-
 sudo add-apt-repository ppa:git-core/ppa
-
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
 
 sudo apt-get update
+sudo apt-get install -y nodejs git mongodb-org
 
-sudo apt-get install -y nodejs git
+
+sudo service mongod start
+sudo service mongod status
+sudo service mongod stop
+
 
 git config --global user.name "zaharchenko-test"
 git config --global user.email "zaharchenko.jeka.v@gmail.com"
@@ -23,18 +28,13 @@ git config --global user.password "0988383057a"
 
 
 sudo npm install -g npm@latest
-
 npm install -g create-react-app
 npm install -g @angular/cli
 		ng serve --host 0.0.0.0 --port 4200
 
-
 wget https://github.com/cdr/code-server/releases/download/v3.7.2/code-server-3.7.2-linux-arm64.tar.gz
-
 tar -xvf ./code-server-3.7.2-linux-arm64.tar.gz
-
 ln -s /lib/code-server-3.7.2-linux-arm64/code-server /bin/
-
 
 
 du -sh /var/cache/apt/archives
