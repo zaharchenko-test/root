33c9b3d8e889a486a475bc1ef35ea74f8e1de445
zaharchenko-test <74099400+zaharchenko-test@users.noreply.github.com>
2020-11-26 23:25:47 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -15,11 +15,13 @@ c='\[\e[1;36m\]'
 w='\[\e[1;37m\]'
 
 # simple kali prompt
-PS1="$r\\]root@localhost :$g [$y \W $g] $r~#  $c"
+#PS1="$r\\]root@localhost :$g [$y \W $g] $r~#  $c"
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]>
 
 # 2nd prompt
-#PS1="\n$r╔═[ $c\@ $r] [ $c\V $r] [ $c$nifce $r] [ $c\W $r] [ $c\j $r]\n$r║ \n$r╚═[$c root@localhost $r] $c>> "
+#PS1="\n$r   ^u^t   ^u^p[ $c\@ $r] [ $c\V $r] [ $c$nifce $r] [ $c\W $r] [ $c\j $r]\n$r   ^u^q \>
 
 # corsor and it's color
 echo -e '\e[3 q'
 echo -ne "\033]12;#00ff00\007"
+LS_COLORS=$LS_COLORS:'di=1;34:ex=1;33:fi=1;36:' ; export LS_COLORS
